538934a354b3e5c8670008e57e4bce2e1a75e518
Dan Wojtowicz <dwojtowi@hawk.iit.edu>
2019-04-02 16:19:24 -0500

    Add picamera skeleton code

--- pi/app.py
+++ pi/app.py
@@ -1,10 +1,39 @@
+#### 0. Imports, initiatilizing variables
+#########################################
+
 import requests
 import time
 import base64
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
 
 server = 'localhost'
 port = '80'
 endpoint = 'upload'
+
+#### 1. Function definitions
+#########################################
+
+def take_image():
+    # Take an image, save it to stream 
+    my_stream = BytesIO()
+    camera = PiCamera()
+    camera.capture(my_stream, 'jpeg')
+
+def translate_image():
+    # Takes stream jpeg and converts it to base64
+
+
+
+#### 2. Primary while loop
+#########################################
+
+while(True):
+    # Take image
+    # Translate image
+    # Send image
+
 
 uploadURL = f"http://{server}:{port}/{endpoint}"
 
@@ -13,7 +42,7 @@ file = open('images/meteor.jpg', 'rb')
 jpeg_text = base64.b64encode(file.read())
 data = {
 	'time': time.time(),
-	'lat': 54.232,
+	'lat': ,
 	'long': -21.311,
 	'image': str(jpeg_text)[1:]
 }
